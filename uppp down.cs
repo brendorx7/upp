a9c351ec4cefacf32dec7deae6d448d437810160
brendorx7 <rafaelbrendosl7@gmail.com>
2024-08-05 16:16:04 -0300

    Add files via upload

---
+++ uppp down.cs
@@ -0,0 +1,26 @@
+﻿
+Console.WriteLine("digite sua idade : ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a <= 12)
+{
+    Console.WriteLine("voce é crianca ");
+}
+
+else if (a <= 17)
+
+{
+    Console.WriteLine("adolescente");
+
+}
+
+else if (a <= 59)
+{
+    Console.WriteLine("adulto");
+
+}
+else
+{
+    Console.WriteLine("idoso");
+
+}
